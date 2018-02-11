f51a85736b5ab4d7304c4f1e1f4cfa8ef1b8a089
ssrahul96 <ssrahul96@gmail.com>
2018-02-11 12:30:57 +0530

    updated tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://ssrahul96:hornet160@ds127888.mlab.com:27888/payment', ['details'])
 
 
 router.get('/pay',function(req,res,next){
-    res.send('Tasks');
+    db.tasks.find(function(err,pay){
+        console.log(db);
+        if(err){
+            console.log(err);
+            return next(err)
+        }
+        res.json(pay);
+    });
 });
 
 module.exports = router;
