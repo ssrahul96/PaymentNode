c6cfe85eccb7bdc02d7b4ccab7acc0e068582246
ssrahul96 <ssrahul96@gmail.com>
2018-02-11 12:41:36 +0530

    updated connection string

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://ssrahul96:hornet160@ds127888.mlab.com:27888/payment', ['details'])
-
+var db = mongojs('mongodb://ssrahul96:hornet160@ds127888.mlab.com:27888/payment',['pay']);
 
 router.get('/pay',function(req,res,next){
-    db.tasks.find(function(err,pay){
+    db.pay.find(function(err,pay){
         console.log(db);
         if(err){
-            console.log(err);
-            return next(err)
+            res.send(err);
         }
         res.json(pay);
     });
