37042c535add31f01373707545c43dc633e095e6
ssrahul96 <ssrahul96@gmail.com>
2018-02-11 12:58:41 +0530

    added paydetail

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,17 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://ssrahul96:hornet160@ds127888.mlab.com:27888/payment',['pay']);
+var db = mongojs('mongodb://ssrahul96:hornet160@ds127888.mlab.com:27888/payment',['paydetails']);
 
-router.get('/pay',function(req,res,next){
-    db.pay.find(function(err,pay){
+//all paydetails
+router.get('/paydetails',function(req,res,next){
+    db.paydetails.find(function(err,paydetails){
         console.log(db);
         if(err){
             res.send(err);
         }
-        res.json(pay);
+        res.json(paydetails);
     });
 });
+
+//pay details
+router.get('/paydetail/:name/:mode', function(req, res, next){
+    db.paydetails.find({pname:req.params.name,ptype:req.params.mode}, function(err, paydetail){
+        if(err){
+            res.send(err);
+        }
+        res.json(paydetail);
+    });
+});
+
 
 module.exports = router;
 
