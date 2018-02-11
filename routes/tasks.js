59491beb89006e1f4840373745de61d9606c5582
ssrahul96 <ssrahul96@gmail.com>
2018-02-11 13:41:52 +0530

    added user function

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,41 +1,90 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://ssrahul96:hornet160@ds127888.mlab.com:27888/payment',['paydetails']);
+
+
+
+//all users
+router.get('/users',function(req,res,next){
+    var token = req.get("auth")
+    console.log(token);
+    if(token != 123456){
+        res.status(403);
+        res.json({
+            "error": "Unauthorised"
+        });
+    }else{
+        var db = mongojs('mongodb://ssrahul96:hornet160@ds127888.mlab.com:27888/payment',['users']);
+        db.users.find(function(err,users){
+            console.log(db);
+            if(err){
+                res.send(err);
+            }
+            res.json(users);
+        });
+    }
+});
+
 
 //all paydetails
 router.get('/paydetails',function(req,res,next){
-    db.paydetails.find(function(err,paydetails){
-        console.log(db);
-        if(err){
-            res.send(err);
-        }
-        res.json(paydetails);
-    });
+    var token = req.get("auth")
+    console.log(token);
+    if(token != 123456){
+        res.status(403);
+        res.json({
+            "error": "Unauthorised"
+        });
+    }else{
+        var db = mongojs('mongodb://ssrahul96:hornet160@ds127888.mlab.com:27888/payment',['paydetails']);
+        db.paydetails.find(function(err,paydetails){
+            console.log(db);
+            if(err){
+                res.send(err);
+            }
+            res.json(paydetails);
+        });
+    }
 });
 
 //pay details
 router.get('/paydetail/:name/:mode', function(req, res, next){
-    db.paydetails.find({pname:req.params.name,ptype:req.params.mode}, function(err, paydetail){
-        if(err){
-            res.send(err);
-        }
-        res.json(paydetail);
-    });
+    var token = req.get("auth")
+
+    if(token != 123456){
+        res.status(403);
+        res.json({
+            "error": "Unauthorised"
+        });
+    }else{
+        var db = mongojs('mongodb://ssrahul96:hornet160@ds127888.mlab.com:27888/payment',['paydetails']);
+        db.paydetails.find({pname:req.params.name,ptype:req.params.mode}, function(err, paydetail){
+            if(err){
+                res.send(err);
+            }
+            res.json(paydetail);
+        });
+    }
 });
 
 //Save Task
 router.post('/paysave', function(req, res, next){
     var paydet = (req.body);
-    console.log(paydet.pname);
+    var token = req.get("auth")
 
-
+    if(token != 123456){
+        res.status(403);
+        res.json({
+            "error": "Unauthorised"
+        });
+    }
     if(paydet.pname === "" || paydet.ptype === "" || paydet.pdesc === "" || paydet.pdate === "" || paydet.pamount === ""){
         res.status(400);
         res.json({
             "error": "Bad Data"
         });
     }else{
+        var db = mongojs('mongodb://ssrahul96:hornet160@ds127888.mlab.com:27888/payment',['paydetails']);
         db.paydetails.save(paydet, function(err, paysave){
             if(err){
                 res.send(err);
