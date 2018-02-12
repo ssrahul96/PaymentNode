88d4a091d17e388dcbde3f4b5330d0381381e841
Rahul S S <rahul.ss@gofrugal.com>
2018-02-12 14:53:14 +0530

    updated openshift port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var port = 3000;
 
 var app = express();
 
-app.set('port', (process.env.PORT || port));
+app.set('port', (process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || port));
 //View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
